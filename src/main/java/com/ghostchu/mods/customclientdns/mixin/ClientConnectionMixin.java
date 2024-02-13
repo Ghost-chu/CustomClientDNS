daf2fa596602e25a86a9244d89f08f5eb5fe1b85
Ghost_chu <ghostchu@111.com>
2024-02-13 16:15:10 +0800

    Pass the hostname correctly

---
+++ src/main/java/com/ghostchu/mods/customclientdns/mixin/ClientConnectionMixin.java
@@ -0,0 +1,71 @@
+package com.ghostchu.mods.customclientdns.mixin;
+
+import com.ghostchu.mods.customclientdns.dns.CustomInetSocketAddress;
+import com.ghostchu.mods.customclientdns.dns.DNSLookupHelper;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.epoll.Epoll;
+import io.netty.channel.epoll.EpollSocketChannel;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import net.minecraft.client.network.*;
+import net.minecraft.network.ClientConnection;
+import net.minecraft.network.NetworkSide;
+import net.minecraft.util.Lazy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+import java.util.Objects;
+import java.util.Optional;
+
+import static net.minecraft.network.ClientConnection.CLIENT_IO_GROUP;
+import static net.minecraft.network.ClientConnection.EPOLL_CLIENT_IO_GROUP;
+
+@Mixin(ClientConnection.class)
+public class ClientConnectionMixin {
+    private Logger logger = LoggerFactory.getLogger("ClientConnectionMixin");
+
+    @Inject(method = "connect(Ljava/net/InetSocketAddress;ZLnet/minecraft/network/ClientConnection;)Lio/netty/channel/ChannelFuture;", at = @At("HEAD"), cancellable = true)
+    private static void connect(InetSocketAddress address, boolean useEpoll, ClientConnection connection, CallbackInfoReturnable<ChannelFuture> cir) {
+        Lazy<? extends EventLoopGroup> lazy;
+        Class<? extends Channel> class_;
+        if (Epoll.isAvailable() && useEpoll) {
+            class_ = EpollSocketChannel.class;
+            lazy = EPOLL_CLIENT_IO_GROUP;
+        } else {
+            class_ = NioSocketChannel.class;
+            lazy = CLIENT_IO_GROUP;
+        }
+        InetAddress addressToUse = address.getAddress();
+        if(address instanceof CustomInetSocketAddress customInetSocketAddress){
+            try {
+                addressToUse = InetAddress.getByName(customInetSocketAddress.getRealAddress());
+            } catch (UnknownHostException e) {
+                e.printStackTrace();
+            }
+        }
+         cir.setReturnValue(new Bootstrap().group(lazy.get()).handler(new ChannelInitializer<>() {
+
+             @Override
+             protected void initChannel(Channel channel) {
+                 try {
+                     channel.config().setOption(ChannelOption.TCP_NODELAY, true);
+                 } catch (ChannelException channelException) {
+                     // empty catch block
+                 }
+                 ChannelPipeline channelPipeline = channel.pipeline().addLast("timeout", new ReadTimeoutHandler(30));
+                 ClientConnection.addHandlers(channelPipeline, NetworkSide.CLIENTBOUND);
+                 channelPipeline.addLast("packet_handler", connection);
+             }
+         }).channel(class_).connect(addressToUse, address.getPort()));
+
+    }
+}
